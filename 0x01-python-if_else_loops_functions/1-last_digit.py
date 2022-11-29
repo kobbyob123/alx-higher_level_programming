62f96ab0df76a61dc6b956f512394d2d38c3e601
kobbyob123 <popolapii@gmail.com>
2022-11-29 23:57:51 +0000

    Added Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-import random
-number = random.randint(-10000, 10000)
 
 if (number < 0) & (((abs(number) % 10) * -1) != 0):
     print(f"Last digit of {number:d} is {(abs(number) % 10) * -1} and is less than 6 and not 0")
