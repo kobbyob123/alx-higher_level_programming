413ce1f110ce8ea7d7d3c33fb433fd323f292ae6
kobbyob123 <popolapii@gmail.com>
2022-12-20 04:10:16 +0000

    Task 1 fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-'''def safe_print_integer(value):
-    try:
-        if value.isdigit() == 0:
-            ret_val = 0
-    except Exception:
-        ret_val = 1
-        print(value)
-    return ret_val'''
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
