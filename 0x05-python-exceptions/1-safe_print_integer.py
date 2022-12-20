e97070806b04825570f5e5dd06666eae50a51815
kobbyob123 <popolapii@gmail.com>
2022-12-20 03:52:38 +0000

    Changed ErrorType to Exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     try:
         if value.isdigit() == 0:
             ret_val = 0
-    except AttributeError:
+    except Exception:
         ret_val = 1
         print(value)
     return ret_val
