1d6331b06a884a075d2b5e6708cc16f2de947f79
kobbyob123 <popolapii@gmail.com>
2022-12-20 05:45:38 +0000

    Pushing all

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,6 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by zero")
             result.append(0)
         finally:
-            1 + 1
             pass
     return result
