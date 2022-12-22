07a0083e8c126473f4d06c9f6e9ad9d84570d3be
Kwabena Obeng <93714286+kobbyob123@users.noreply.github.com>
2022-12-22 00:28:23 +0000

    finished Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,4 +8,9 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-
+        if size.isdigit():
+            pass
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
